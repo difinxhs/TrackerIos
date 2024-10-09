64f144c3de17426a95a6e9f33f7c6210c08fbbda
vl ad <8.kchayyy@gmail.com>
2024-10-09 22:36:20 +0200

    проект подготовлен под верстку кодом

--- TrackerIos/AppDelegate.swift
+++ TrackerIos/AppDelegate.swift
@@ -1,20 +1,15 @@
-//
-//  AppDelegate.swift
-//  TrackerIos
-//
-//  Created by Alex Sacopini on 7.10.24..
-//
-
 import UIKit
 import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
