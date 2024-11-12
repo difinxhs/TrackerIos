085aece49ce21a2e96d651931613974aef7287c3
vl ad <8.kchayyy@gmail.com>
2024-11-12 19:29:20 +0100

    сверстал ячейку

--- TrackerIos/Trackers/TrackersCollectionCell.swift
+++ TrackerIos/Trackers/TrackersCollectionCell.swift
@@ -1,23 +1,93 @@
 import UIKit
 
 final class TrackersCollectionCell: UICollectionViewCell {
-    let titleLabel: UILabel = UILabel()
+    private let emoji: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(named: "White")
+        view.backgroundColor?.withAlphaComponent(0.3)
+        view.layer.cornerRadius = 12
+        view.clipsToBounds = true
+        
+        let emojiLabel = UILabel()
+        emojiLabel.text = "❤️"
+        emojiLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(emojiLabel)
+        NSLayoutConstraint.activate([
+            emojiLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emojiLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        return view
+    } ()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Tracker"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = UIColor(named: "White")
+        return label
+    } ()
+    
+    private let counterLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 день"
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = UIColor(named: "Black")
+        return label
+    } ()
+    
+    private let plusButton = UIButton.systemButton(
+        with: UIImage(systemName: "plus")!,
+        target: TrackersCollectionCell.self,
+        action: nil
+        
+    )
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        contentView.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-        ])
-        
-        
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupView() {
+        self.backgroundColor = UIColor.green
+        self.layer.cornerRadius = 12
+        
+        addSubview(emoji)
+        addSubview(titleLabel)
+        addSubview(counterLabel)
+        addSubview(plusButton)
+        plusButton.backgroundColor = UIColor.green
+        plusButton.layer.cornerRadius = 17
+        
+        emoji.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        counterLabel.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // emoji
+            emoji.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            emoji.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+            emoji.widthAnchor.constraint(equalToConstant: 24),
+            emoji.heightAnchor.constraint(equalToConstant: 24),
+            
+            // titleLabel
+            titleLabel.leadingAnchor.constraint(equalTo: emoji.trailingAnchor, constant: 8),
+            titleLabel.centerYAnchor.constraint(equalTo: emoji.centerYAnchor),
+            
+            // counterLabel
+            counterLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            counterLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            
+            // plusButton
+            plusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            plusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            plusButton.widthAnchor.constraint(equalToConstant: 32),
+            plusButton.heightAnchor.constraint(equalToConstant: 32)
+        ])
+    }
+    
 }
