dff3f7c5f60f95101d0366504e0b03797393406e
Vladislav Belolipetskii <>
2025-01-30 20:57:15 +0100

    Добавлен онбординг

---
+++ TrackerIos/Onboarding/OnboardingPageViewController.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+class OnboardingPageViewController: UIViewController {
+    
+    private lazy var backgroundImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(named: "Black")
+        label.font = .boldSystemFont(ofSize: 32)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var skipButton: ActionButton = {
+        let button = ActionButton(type: .system)
+        button.addTarget(self, action: #selector(skipButtonDidTap), for: .touchUpInside)
+        button.setTitle("Вот это технологии!", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var labelOffset: CGFloat = {
+        return UIScreen.main.bounds.width <= 320 ? 24 : 64
+    }()
+    private var onCompletion: (() -> Void)?
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(backgroundImage)
+        view.addSubview(textLabel)
+        view.addSubview(skipButton)
+        
+        setupConstraints()
+    }
+    
+    // MARK: - Private Methods
+    func config(text: String, background: UIImage, onCompletion: (() -> Void)?) {
+        backgroundImage.image = background
+        textLabel.text = text
+        self.onCompletion = onCompletion
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: labelOffset),
+            
+            skipButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            skipButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            skipButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            skipButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
+    // MARK: - Actions
+    @objc private func skipButtonDidTap(_ sender: UIButton) {
+        onCompletion?()
+    }
+    
+}
---
+++ TrackerIos/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+final class OnboardingViewController: UIPageViewController {
+    
+    lazy var pages: [UIViewController] = {
+        let onCompletion: () -> Void = { [weak self] in
+            self?.skipOnboarding()
+        }
+        
+        let first = OnboardingPageViewController()
+        first.config(text: "Отслеживайте только то, что хотите",
+                     background: UIImage(named: "Background1") ?? UIImage(),
+                     onCompletion: onCompletion)
+        
+        let second = OnboardingPageViewController()
+        second.config(text: "Даже если это не литры воды и йога",
+                      background: UIImage(named: "Background2") ?? UIImage(),
+                      onCompletion: onCompletion)
+        
+        return [first, second]
+    }()
+    
+    lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.numberOfPages = pages.count
+        pageControl.currentPage = 0
+        
+        pageControl.currentPageIndicatorTintColor = UIColor(named: "Black")
+        pageControl.pageIndicatorTintColor = UIColor(named: "Black")?.withAlphaComponent(0.3)
+        pageControl.isUserInteractionEnabled = false
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        return pageControl
+    }()
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        delegate = self
+        
+        if let first = pages.first {
+            setViewControllers([first], direction: .forward, animated: true, completion: nil)
+        }
+        
+        view.addSubview(pageControl)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -134),
+            pageControl.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    private func skipOnboarding() {
+        if let window = view.window {
+            window.rootViewController = TabBarController()
+        }
+    }
+    
+}
+
+// MARK: - UIPageViewControllerDataSource
+
+extension OnboardingViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        //возвращаем предыдущий (относительно переданного viewController) дочерний контроллер
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        //возвращаем следующий (относительно переданного viewController) дочерний контроллер
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else {
+            return nil
+        }
+        
+        return pages[nextIndex]
+    }
+    
+}
+
+// MARK: - UIPageViewControllerDelegate
+
+extension OnboardingViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+        if let currentViewController = pageViewController.viewControllers?.first,
+           let currentIndex = pages.firstIndex(of: currentViewController) {
+            pageControl.currentPage = currentIndex
+        }
+    }
+}
